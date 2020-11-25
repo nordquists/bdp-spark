69ff39cfa6732b2cad2c465c7a4689ad437e450c
Sean Nordquist <srn334@nyu.edu>
2020-11-25 15:54:02 -0800

    d

--- main.py
+++ main.py
@@ -54,15 +54,12 @@ temp = pipeline.fit(ts).transform(ts)
 # gc.collect()
 
 
-
-
-
 # temp.createOrReplaceTempView("temp_table")
 # hive_context.sql("create table feature_table as select * from temp_table")
 
-# result = temp.rdd\
-#     .map(tuple).map(lambda (repo, week, score, repo_indexed, repo_indexed_encoded, features): "{},{},{}".format(repo,str(features),str(score)))\
-#     .saveAsTextFile("hdfs://dumbo/user/srn334/final/test_output1/")
+result = temp.rdd\
+    .map(tuple).map(lambda (repo, week, score, repo_indexed, repo_indexed_encoded, features): "{},{},{}".format(repo,str(features),str(score)))\
+    .saveAsTextFile("hdfs://dumbo/user/srn334/final/test_output1/")
 
 
 gbt = GBTRegressor(featuresCol='features', labelCol='score')
