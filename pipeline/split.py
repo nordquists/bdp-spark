67a4fab6d2fdf50a0b836aceacfb278b241ec219
Sean Nordquist <srn334@nyu.edu>
2020-11-25 15:26:00 -0800

    d

--- pipeline/split.py
+++ pipeline/split.py
@@ -1,10 +1,10 @@
-from pyspark.sql import functions as f
+import pyspark.sql.functions as f
 from pipeline.config import TRAIN_WEEKS, TARGET
 
 
 def get_train_split(df):
-    return df.filter(f.Column('week').between(0, TRAIN_WEEKS)).withColumn(TARGET, f.Column(TARGET))
+    return df.filter(f.col('week').between(0, TRAIN_WEEKS)).withColumn(TARGET, f.col(TARGET))
 
 
 def get_eval_split(df):
-    return df.filter(f.Column('week') > TRAIN_WEEKS).withColumn(TARGET, f.Column(TARGET))
+    return df.filter(f.col('week') > TRAIN_WEEKS).withColumn(TARGET, f.col(TARGET))
