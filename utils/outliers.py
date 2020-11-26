28fb2f97e19048041f38809df68ab3de778836b4
Sean Nordquist <srn334@nyu.edu>
2020-11-26 08:16:59 -0800

    d

--- utils/outliers.py
+++ utils/outliers.py
@@ -4,6 +4,6 @@ import numpy as np
 
 
 def exclude_outliers(y, df):
-    quartile1, quartile2 = np.quantile(y, 0.25), np.quantile(y, 0.75)
+    quartile1, quartile2 = np.percentile(y, 25), np.percentile(y, 75)
     filtered_df = df.filter(f.col('score').between(quartile1, quartile2))
     return filtered_df
