f5aea00de502745c6a273d64ff47429dbf97b948
Sean Nordquist <srn334@nyu.edu>
2020-11-26 14:05:58 -0800

    d

--- regression/runner.py
+++ regression/runner.py
@@ -1,5 +1,16 @@
 """
 
-    The purpose of the runner is to
+    The purpose of the runner is to parallelize the running of the linear regression models.
+
+    Broadly:
+
+    MAP(each repo name -> linearRegression(repo_name))
 
 """
+
+
+
+
+
+
+def map_linear_regression(repo_name):
