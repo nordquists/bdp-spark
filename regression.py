609e1bc63e06829f98fbb996d1e2f181630b61c2
Sean Nordquist <srn334@nyu.edu>
2020-11-26 14:55:08 -0800

    d

--- regression.py
+++ regression.py
@@ -13,26 +13,27 @@ sc.setLogLevel("OFF")
 
 def map_linear_regression(repo_name):
     print(repo_name)
-    global hive_context
-    global INPUT_TABLE
-
-    # # We have to
-    # ts = hive_context.table("srn334.{}".format(INPUT_TABLE))
-    # ts.registerTempTable('{}'.format(INPUT_TABLE))
-
-    ts = hive_context.sql("SELECT * FROM {} where repo = '{}'".format(INPUT_TABLE, repo_name))
-
-    lr = LinearRegression()
-
-    x = np.array(ts.select('day').collect()).flatten()
-    y = np.array(ts.select('score').collect()).flatten()
-
-    predictions = lr.predict(x)
-    rmse, r2 = lr.evaluate(predictions, y)
-    slope = lr.get_slope()
-    intercept = lr.get_intercept()
-
-    return "{},{},{},{}".format(repo_name, slope, intercept, r2)
+    # global hive_context
+    # global INPUT_TABLE
+    #
+    # # # We have to
+    # # ts = hive_context.table("srn334.{}".format(INPUT_TABLE))
+    # # ts.registerTempTable('{}'.format(INPUT_TABLE))
+    #
+    # ts = hive_context.sql("SELECT * FROM {} where repo = '{}'".format(INPUT_TABLE, repo_name))
+    #
+    # lr = LinearRegression()
+    #
+    # x = np.array(ts.select('day').collect()).flatten()
+    # y = np.array(ts.select('score').collect()).flatten()
+    #
+    # predictions = lr.predict(x)
+    # rmse, r2 = lr.evaluate(predictions, y)
+    # slope = lr.get_slope()
+    # intercept = lr.get_intercept()
+    #
+    # return "{},{},{},{}".format(repo_name, slope, intercept, r2)
+    return ""
 
 def do_regression(repos):
     print("STARTING REGRESSION MAP: --------------------------------")
@@ -47,11 +48,13 @@ ts.registerTempTable('{}'.format(INPUT_TABLE))
 ts = hive_context.sql("SELECT * FROM {} WHERE month < {}".format(INPUT_TABLE, str(TRAINING_CUT_OFF)))
 
 # First we find all of the repositories that we will run a regression on.
-print("LOADING REPOSITORIES: -------------------------------")
+print("LOADING REPOSITORIES: ------------------------------------")
 repos = ts.rdd.map(tuple).map(lambda (x, y, z): x)
 print("LOADED {} REPOSITORIES: -----------------------------".format(repos.count()))
 
 result = do_regression(repos)
+
+print(OUTPUT_DIR)
 
 result.saveAsTextFile(OUTPUT_DIR)
 
