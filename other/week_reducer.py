3ff2c57c8d1e893eeaee453707791e3e44231951
Sean Nordquist <srn334@nyu.edu>
2020-11-26 09:56:04 -0800

    d

--- other/week_reducer.py
+++ other/week_reducer.py
@@ -8,4 +8,4 @@ newdata = output.map(lambda line: line.split(","))
 
 result = newdata.map(lambda row: ("{},{},{}," .format(row[0], row[1], datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S %Z').timetuple().tm_yday),row[4])).reduceByKey(lambda a, b: a + b)
 
-result.saveAsTextFile("hdfs://dumbo/user/srn334/final/indexed_data/")
+result.saveAsTextFile("hdfs://dumbo/user/srn334/final/indexed_data_days/")
