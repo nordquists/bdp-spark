36d7a5cd8e25d705e0387aabae86f60c2016a8b8
Sean Nordquist <srn334@nyu.edu>
2020-11-27 05:56:17 -0800

    d

---
+++ cumulative.py
@@ -0,0 +1,29 @@
+"""
+
+Partition -> order by -> cumsum
+
+"""
+import sys
+from pyspark import SparkContext
+from pyspark.sql import HiveContext
+from pyspark.sql.window import Window
+import pyspark.sql.functions as f
+
+
+INPUT_DIR = "hdfs://dumbo/user/srn334/final/output/part-r-00000"
+OUTPUT_DIR = "hdfs://dumbo/user/srn334/final/preprocessed_weekly/"
+
+sc = SparkContext.getOrCreate()
+hive_context = HiveContext(sc)
+
+sc.setLogLevel("WARN")
+
+rdd = sc.textFile(INPUT_DIR)
+
+ts = rdd.map(lambda line: line.split(",")).toDF(["repo", "week", "score"])
+
+ts = ts.fillna({'score': 0, 'day': 0, 'repo': ''})
+
+
+cum_sum = ts.withColumn('cumsum', f.sum(f.col("score")).over(Window.partitionBy('repo').orderBy(f.col("week")).rowsBetween(-sys.maxsize, 0)))
+cum_sum.show(100)
