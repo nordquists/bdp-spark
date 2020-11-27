6bb4a291a8609e1e6639fed35c48100f2fe90088
Sean Nordquist <srn334@nyu.edu>
2020-11-27 07:59:41 -0800

    d

---
+++ final_index.py
@@ -0,0 +1,34 @@
+from pyspark import SparkContext
+from pyspark.sql import HiveContext
+import numpy as np
+import pyspark.sql.functions as f
+
+INPUT_TABLE = "ts_weekly_cumulative_preprocessed"
+OUTPUT_DIR = "hdfs://dumbo/user/srn334/final/final_index/"
+
+sc = SparkContext.getOrCreate()
+hive_context = HiveContext(sc)
+sc.setLogLevel("OFF")
+
+
+
+def index_mapper(line):
+    repo_name, slope, intercept, r2, entries, integral, derivative = line
+
+    index = (0.4 * integral + 0.6 * derivative)
+    index *= entries / 52
+    index += derivative / integral
+
+    return "{},{}".format(repo_name, index)
+
+
+
+ts = hive_context.table("srn334.{}".format(INPUT_TABLE))
+ts.registerTempTable('{}'.format(INPUT_TABLE))
+
+ts = hive_context.sql("SELECT * FROM {}".format(INPUT_TABLE))
+
+
+result = ts.rdd.map(tuple).map(index_mapper)
+
+result.saveAsTextFile(OUTPUT_DIR)
