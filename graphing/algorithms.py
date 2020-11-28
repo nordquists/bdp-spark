dfe00ed5595c8b0102c6f36426abef6e651839d5
Sean Nordquist <srn334@nyu.edu>
2020-11-28 15:21:14 -0800

    d

--- graphing/algorithms.py
+++ graphing/algorithms.py
@@ -25,9 +25,8 @@ def run_algorithms(algorithms, x, y, x_hat):
     for algorithm in algorithms:
         if algorithm == 0:
             fit = arima(y)
-            pred = fit.predict(1, x_hat[-1], typ='levels')
+            pred = fit.predict(1, 50, typ='levels')
             y_1, y_hat = pred[:x[-1]], pred[x_hat[0]:]
-            print(len(pred))
             results.append([algorithm, (np.append(x, x_hat, 0), pred)])
         elif algorithm == 1:
             y_1 = movingaverage(y, 3)
