140dae803a93e3b3dd065c5287aa6819ddc9c616
Sean Nordquist <srn334@nyu.edu>
2020-11-28 18:21:33 -0800

    d

--- graphing/split.py
+++ graphing/split.py
@@ -2,8 +2,8 @@ import pyspark.sql.functions as f
 
 
 def get_train_split(df):
-    return df.filter(f.col('week').between(0, 35)).withColumn("cumsum", f.col("cumsum"))
+    return df.filter(f.col('week').between(0, 50)).withColumn("cumsum", f.col("cumsum"))
 
 
 def get_eval_split(df):
-    return df.filter(f.col('week') > 35).withColumn("cumsum", f.col("cumsum"))
+    return df.filter(f.col('week') > 50).withColumn("cumsum", f.col("cumsum"))
