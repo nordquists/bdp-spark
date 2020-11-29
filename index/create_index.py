5fa433400730eeebc8876d2efd3fb6a51c349656
Sean Nordquist <srn334@nyu.edu>
2020-11-28 21:57:53 -0800

    d

--- index/create_index.py
+++ index/create_index.py
@@ -1,4 +1,3 @@
-from linear import LinearRegression
 from pyspark import SparkContext
 from pyspark.sql import HiveContext
 import numpy as np
@@ -12,6 +11,50 @@ sc = SparkContext.getOrCreate()
 hive_context = HiveContext(sc)
 sc.setLogLevel("OFF")
 
+# ----------------------------------------------------------------------------------------------------------------
+"""
+    I am sorry this is here, python packages was not letting me import it.
+"""
+
+class LinearRegression:
+    def __init__(self):
+        self.__slope = None
+        self.__intercept = None
+        self.__r_squared = None
+        self.__p_value = None
+        self.__std_err = None
+        self.__fitted = None
+
+    def fit(self, x, y, order=1):
+        self.__fitted = np.polyfit(x, y, order)
+        self.__slope = self.__fitted[0]
+        self.__intercept = self.__fitted[1]
+
+    def predict(self, x):
+        if self.__fitted is None:
+            return None
+
+        predictions = np.polyval(self.__fitted, x)
+
+        return predictions
+
+    def evaluate(self, predictions, y):
+        abs_error = predictions - y
+        squared_error = np.square(abs_error)
+        mean_squared_error = np.mean(squared_error)
+        rmse = np.sqrt(mean_squared_error)
+        r2 = 1.0 - (np.var(abs_error) / np.var(y))
+
+        return rmse, r2
+
+    def get_slope(self):
+        return self.__slope
+
+    def get_intercept(self):
+        return self.__intercept
+
+
+# ----------------------------------------------------------------------------------------------------------------
 
 def map_linear_regression_derivative(line):
     # # We have to
@@ -52,6 +95,8 @@ def index_mapper(line):
 
     return (linked, index)
 
+# ----------------------------------------------------------------------------------------------------------------
+
 ts = hive_context.table("srn334.{}".format(INPUT_TABLE))
 ts.registerTempTable('{}'.format(INPUT_TABLE))
 
@@ -70,5 +115,7 @@ result = result.map(index_mapper)
 result = result.toDF(['repo', 'index']).orderBy('index', ascending=False)
 
 result = result.rdd.map(tuple).map(lambda (repo_name, index): "{},{}".format(repo_name, index))
+
+# ----------------------------------------------------------------------------------------------------------------
 
 result.saveAsTextFile(OUTPUT_DIR)
