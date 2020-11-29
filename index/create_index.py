e16d275b76f4e2c5aab789a80a36706f5e10161a
Sean Nordquist <srn334@nyu.edu>
2020-11-28 21:59:30 -0800

    d

--- index/create_index.py
+++ index/create_index.py
@@ -80,12 +80,10 @@ def map_linear_regression_derivative(line):
     else:
         dydx = slope
 
-    return "{},{},{},{},{},{},{}".format(repo_name, slope, intercept, r2, len(x), cumsum[-1], dydx)
+    return repo_name, slope, intercept, r2, len(x), cumsum[-1], dydx
 
 
-def index_mapper(line):
-    repo_name, slope, intercept, r2, entries, integral, derivative = line
-
+def index_mapper(repo_name, slope, intercept, r2, entries, integral, derivative):
     index = (0.4 * float(integral) + 0.6 * float(derivative))
     index *= float(entries) / 52.0
     if integral:
