27e0a3613b862efe6e6b59cef664087d320e53b6
Sean Nordquist <srn334@nyu.edu>
2020-11-28 18:03:14 -0800

    d

--- graphing/outliers.py
+++ graphing/outliers.py
@@ -4,5 +4,5 @@ import numpy as np
 
 def exclude_outliers(y, df):
     quartile1, quartile2 = np.percentile(y, 25), np.percentile(y, 75)
-    filtered_df = df.filter(f.col('score').between(quartile1, quartile2))
+    filtered_df = df.filter(f.col('cumsum').between(quartile1, quartile2))
     return filtered_df
