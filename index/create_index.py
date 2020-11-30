458878ebc2c87e6079b7f5e38bfcf977b3236e71
Sean Nordquist <srn334@nyu.edu>
2020-11-29 16:06:40 -0800

    d

--- index/create_index.py
+++ index/create_index.py
@@ -108,7 +108,7 @@ df = ts.groupBy("repo").agg(f.collect_list("week"), f.collect_list("cumsum"))
 # We do the mapping, each map task is a linear regression task.
 result = df.rdd.map(tuple).map(map_linear_regression_derivative)
 
-result.rdd.map(tuple).saveAsTextFile("hdfs://dumbo/user/srn334/final/eval/")
+result.saveAsTextFile("hdfs://dumbo/user/srn334/final/eval/")
 
 # # Now we use those results to calculate the index
 # result = result.map(index_mapper)
