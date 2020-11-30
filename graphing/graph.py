fc084613e53730d3dac614e1654ca1f7f2dd0c93
Sean Nordquist <srn334@nyu.edu>
2020-11-29 17:38:22 -0800

    d

--- graphing/graph.py
+++ graphing/graph.py
@@ -37,7 +37,7 @@ for i in range(len(TABLEAU_20)):
 # -----------------------------------------------------------------
 
 # Adjustables
-REPO_NAME = "sindresorhus/awesome"
+REPO_NAME = "willmcgugan/rich"
 TABLE_NAME = "weekly_cumulative"
 TYPE = "score"
 TO_PLOT = [
