322f601503f3bc74e1d75a797640304f7af11af3
Sean Nordquist <srn334@nyu.edu>
2020-12-01 15:20:37 -0800

    d

--- graphing/graph.py
+++ graphing/graph.py
@@ -37,7 +37,7 @@ for i in range(len(TABLEAU_20)):
 # -----------------------------------------------------------------
 
 # Adjustables
-REPO_NAME = "kamranahmedse/developer-roadmap"
+REPO_NAME = "rust-lang/rust"
 TABLE_NAME = "weekly_cumulative"
 TYPE = "cumsum"
 TO_PLOT = [
